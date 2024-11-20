bc127d938195723c5d93c7840efbdfd94e31e7b1
Kornjat <jorgetorresmena@outlook.es>
2024-11-19 23:10:21 -0600

    Agregue Producto, Proveedor, Categoria de Producto

--- layouts/Navbar.php
+++ layouts/Navbar.php
@@ -129,17 +129,26 @@ if (!isset($_SESSION['usuario'])) {
                                     <a class="dropdown-item" href="./alerts.html">
                                         Gestion de inventario
                                     </a>
-                                    <a class="dropdown-item" href="./alerts.html">
-                                        Proveedores
-                                    </a>
                                     <a class="dropdown-item" href="./accordion.html">
                                         Movimientos
                                     </a>
-                                    <a class="dropdown-item" href="./accordion.html">
-                                        Productos
+                                    <a class="dropdown-item" href="../vistas/registrar_proveedor.php">
+                                        Registrar Proveedores
                                     </a>
-                                    <a class="dropdown-item" href="./accordion.html">
-                                        Categoria de productos
+                                    <a class="dropdown-item" href="../vistas/lista-proveedor.php">
+                                        Gestionar Proveedores
+                                    </a>
+                                    <a class="dropdown-item" href="../vistas/registrar_producto.php">
+                                        Registrar Productos
+                                    </a>
+                                    <a class="dropdown-item" href="../vistas/lista-producto.php">
+                                        Gestionar Productos
+                                    </a>
+                                    <a class="dropdown-item" href="../vistas/registrar_categoria.php">
+                                        Registrar Categoria de productos
+                                    </a>
+                                    <a class="dropdown-item" href="../vistas/lista-categoria.php">
+                                        Gestionar Categoria de productos
                                     </a>
                                 </div>
                             </div>
